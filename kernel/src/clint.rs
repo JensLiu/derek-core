8ee091e2eab62c86b19dc022e4b4183cac097b0f
Jens Liu <jensliu29@outlook.de>
2024-03-05 15:18:46 +0800

    new timer scratch type

--- kernel/src/clint.rs
+++ kernel/src/clint.rs
@@ -1,46 +1,68 @@
+use crate::mm::layout::{CLINT_MTIMECMP_BASE, CLINT_MTIME_BASE};
+use crate::{
+    arch::hart_id,
+    symbols::{__timervec, NCPUS, SCHEDULER_INTERVAL},
+};
 use riscv::register::*;
-use crate::{arch::hart_id, symbols::{timervec, NCPUS, SCHEDULER_INTERVAL}};
 
 // core local interruptor (CLINT), which contains the timer.
 pub const CLINT_BASE: usize = 0x200_0000;
-pub const CLINT_MTIMECMP_BASE: usize = CLINT_BASE + 0x4000; // mechine-level time compare
-pub const CLINT_MTIME_BASE: usize = CLINT_BASE + 0xbff8;
 
 #[allow(non_snake_case)]
-pub const fn CLINT_TIMECMP(hart: usize) -> usize { CLINT_MTIMECMP_BASE + 8*hart }
+pub const fn CLINT_TIMECMP(hart: usize) -> usize {
+    CLINT_MTIMECMP_BASE + 8 * hart
+}
 
 /// scratch area for timer trap to save information: 64 bytes per core
-static mut TIMER_SCRATCH: [[u64; 8]; NCPUS] = [[0; 8]; NCPUS];
+// static mut TIMER_SCRATCH: [[u64; 8]; NCPUS] = [[0; 8]; NCPUS];
+/// this init the timer scratch for each cpu
+
+/// refer to `__timervec` in `src/asm/kernelvec.S`
+#[repr(C)]
+#[derive(Clone, Copy)]
+struct TimerScratch {
+    tmp_regs: [usize; 3],
+    mtime_addr: usize,
+    mtimecmp_addr: usize,
+    interval: usize,
+}
+
+// We allocate a `TimerScratch` for each CPU
+// TODO: don't use static, other options?
+static mut TIMER_SCRATCHES: [TimerScratch; NCPUS] = [TimerScratch {
+    tmp_regs: [0; 3],
+    mtime_addr: 0,
+    mtimecmp_addr: 0,
+    interval: 0,
+}; NCPUS];
 
 pub unsafe fn timer_init() {
     let id = hart_id();
     let mtime = CLINT_MTIME_BASE as *mut u64;
 
     // ask CLITN for timer interrupt
-    let interval = SCHEDULER_INTERVAL as u64;   // cycles; about 1/10th second in qemu
+    let interval = SCHEDULER_INTERVAL as u64; // cycles; about 1/10th second in qemu
     let mtimecmp = CLINT_TIMECMP(id) as *mut u64;
-    // our timer interrupt will occur at `mtime` + `interval`,
-    //  where `mtime` is the current time
+    // our timer interrupt will occur at `mtime` + `interval`, where `mtime` is the current time
     mtimecmp.write_volatile(mtime.read_volatile() + interval);
-    
+
     // prepare information in scratch[] for timervec
     // scratch[0..2]: space for timervec to save registers(3 * size): because timervec uses these registers
     // scratch[3]: adress for CLINT MTIME register
     // scratch[4]: address for CLINT MTIMECMP register
     // scratch[5]: desired interval (in cycles) between timer interrupts
-    let scratch = &mut TIMER_SCRATCH[id];
-    mscratch::write(scratch.as_mut_ptr() as usize); // mscratch register is only accessable in M-mode
-    scratch[3] = mtime as u64;
-    scratch[4] = mtimecmp as u64;
-    scratch[5] = interval;
+    let scratch = &mut TIMER_SCRATCHES[id];
+    mscratch::write(scratch as *const TimerScratch as usize); // mscratch register is only accessable in M-mode
+    scratch.mtime_addr = mtime as usize;
+    scratch.mtimecmp_addr = mtimecmp as usize;
+    scratch.interval = interval as usize;
 
-    // set M-mode trap handler to `timervec` in `kernelvec.S`
-    mtvec::write(timervec as usize, mtvec::TrapMode::Direct);
+    // set M-mode trap handler to `__timervec` in `kernelvec.S`
+    mtvec::write(__timervec as usize, mtvec::TrapMode::Direct);
 
     // enable M-mode interrupts
     mstatus::set_mie(); // `mie` (machine interrupt enabled) bit in `mstatus` register
 
     // enable M-mode timer interrupt
-    mie::set_mtimer()   // `mtimer` bit in `mie` register
-
+    mie::set_mtimer() // `mtimer` bit in `mie` register
 }
